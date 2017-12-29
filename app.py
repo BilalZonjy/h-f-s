398ba5f445c833e65bb6749a70af24a97b54e250
Bilal Zonjy <bxz142@case.edu>
2017-12-29 12:20:58 -0500

    CORS is added
    Signed-off-by: Bilal Zonjy <bxz142@case.edu>

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from flask import Flask ,jsonify,request
 from sklearn.externals import joblib
 import numpy as np 
+from flask_cors import CORS
 
 loaded_model = joblib.load('model.pkl')
 X_test = np.array([[   6. ,    148.   ,   72.  ,    35.   ,    0.    ,  33.6     , 0.627  , 50.   ]])
 # X_test = np.array([[   0. ,    0.   ,   0.  ,    0.   ,    0.    ,  0     , 0.627  , 0.   ]])
 app = Flask(__name__)
-
+CORS(app)
 
 @app.route('/')     
 def list_store():
